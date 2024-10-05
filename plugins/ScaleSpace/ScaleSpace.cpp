de53c96d0611607883f7f4ff64efb7db5cab8da5
eventual-recluse <eventual_recluse@outlook.com>
2024-10-05 04:19:33 +0100

    Tidied file opening code

--- plugins/ScaleSpace/ScaleSpace.cpp
+++ plugins/ScaleSpace/ScaleSpace.cpp
@@ -245,94 +245,38 @@ protected:
     void setState(const char* key, const char* value) override
     {
 		// note: internal states seem to get set as soon as file chosen by file dialog, and could end up being anything
-		
-        //d_stdout("DSP setState %s %s", key, value);
-        int fileId = -1;
 
         /**/ if (std::strcmp(key, "scl_file_1") == 0)
         {
-		    fileId = 0;
+		    loadScl(tuning1, value);
 		}
         else if (std::strcmp(key, "scl_file_2") == 0)
         {   
-			fileId = 1;
+			loadScl(tuning2, value);
 		}
         else if (std::strcmp(key, "scl_file_3") == 0)
 	    {
-            fileId = 2;
+            loadScl(tuning3, value);
         }
         else if (std::strcmp(key, "scl_file_4") == 0)
 	    {
-            fileId = 3;
+            loadScl(tuning4, value);
         }
         else if (std::strcmp(key, "kbm_file_1") == 0)
 	    {
-            fileId = 4;
+            loadKbm(tuning1, value);
         }
         else if (std::strcmp(key, "kbm_file_2") == 0)
 	    {
-            fileId = 5;
+            loadKbm(tuning2, value);
         }
         else if (std::strcmp(key, "kbm_file_3") == 0)
 	    {
-            fileId = 6;
+            loadKbm(tuning3, value);
         }
         else if (std::strcmp(key, "kbm_file_4") == 0)
 	    {
-            fileId = 7;
-        }
-        
-        if (fileId == -1)
-            return;
-
-        if (FILE* const fh = fopen(value, "r"))
-        {
-			
-			if (!value)
-			{    
-			    fclose(fh);
-			    return; 
-			}
-			
-			//d_stdout(value);
-			
-			//String filename(value);
-			
-
-			if (fileId == 0)
-			{	
-				loadScl(tuning1, value);
-			}
-			else if (fileId == 1)
-			{
-				loadScl(tuning2, value);
-			}
-			else if (fileId == 2)
-			{
-				loadScl(tuning3, value);
-			}	
-			else if (fileId == 3)
-			{	
-				loadScl(tuning4, value);
-			}		
-			else if (fileId == 4)
-			{
-				loadKbm(tuning1, value);
-			}
-			else if (fileId == 5)
-			{
-				loadKbm(tuning2, value);
-			}
-			else if (fileId == 6)
-			{
-				loadKbm(tuning3, value);
-			}
-			else if (fileId == 7)
-			{
-				loadKbm(tuning4, value);
-			}
-			
-            fclose(fh);
+            loadKbm(tuning4, value);
         }
     }
     
